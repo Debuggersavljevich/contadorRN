1b1b7de8219f698458518b0daa268fa460b4e7c9
debuggersavljevich <sbogosavljevich@gmail.com>
2023-01-31 00:17:20 -0300

    funcionalidad de suma y resta

---
+++ src/screens/ContadorScreen.tsx
@@ -0,0 +1,75 @@
+import {useState} from 'react'
+import { View, Text, TouchableOpacity, StyleSheet, TouchableWithoutFeedback } from 'react-native'
+
+export const ContadorScreen = () => {
+  
+    const [contador, setContador] = useState(10)
+
+  return (
+    <View style={style.container}>
+    <Text style={style.title}>
+            Contador: {contador}
+    </Text>
+    
+    
+    <TouchableOpacity
+    onPress={()=> setContador(contador+1)}
+    style={style.fabLocationBR}
+    >
+    <View style={style.fab}>
+        <Text style={style.fabChildren}>
+            +1
+        </Text>
+    </View>
+    </TouchableOpacity>
+    
+    <TouchableOpacity
+    onPress={()=> setContador(contador-1)}
+    style={style.fabLocationBL}
+    >
+    <View style={style.fab}>
+        <Text style={style.fabChildren}>
+            -1
+        </Text>
+    </View>
+    </TouchableOpacity>
+    
+    </View>
+  )
+}
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center'
+    },
+    title: {
+        textAlign: 'center',
+        fontSize: 40,
+        top: -15
+    },
+    fabLocationBR: {
+        position: 'absolute',
+        bottom: 25,
+        right: 25
+    },
+    fabLocationBL: {
+        position: 'absolute',
+        bottom: 25,
+        left: 25
+    },
+    fab: {
+        backgroundColor: '#5856d6',
+        width: 60,
+        height: 60,
+        borderRadius: 100,
+        justifyContent: 'center'
+    },
+    fabChildren: {
+        color: 'white',
+        fontSize: 20,
+        fontWeight: 'bold',
+        alignSelf: 'center'
+    }
+})
+
