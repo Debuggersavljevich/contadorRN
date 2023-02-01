0a66b6b7d6987ead18e560960a7eedc7edaa041b
debuggersavljevich <sbogosavljevich@gmail.com>
2023-02-01 20:53:22 -0300

    fix de errores

--- src/components/Fab.tsx
+++ src/components/Fab.tsx
@@ -1,83 +1,88 @@
-import {useState} from 'react'
-import { TouchableOpacity, View, Text, StyleSheet, TouchableNativeFeedback} from 'react-native'
+import React from 'react'
+import { TouchableOpacity, View, Text, StyleSheet, TouchableNativeFeedback, Platform } from 'react-native';
 
 
 interface Props {
-    title: string,
-    position?: 'bl' | 'br'
-    onPress: () => void,
+    title: string;
+    position?: 'br' | 'bl';
+    onPress: () => void;
 }
 
-export const Fab = ({title, onPress, position = 'br'}: Props) => {
+export const Fab = ({ title, onPress, position = 'br' }: Props ) => {
 
-    
+    const ios = () => {
+        return (
+            <TouchableOpacity
+                onPress={ onPress }
+                activeOpacity={ 0.75 }
+                style={[ 
+                    styles.fabLocation,  
+                    ( position === 'bl' ) ? styles.left : styles.right
+                ]}
+            >
+                <View style={ styles.fab }>
+                    <Text style={ styles.fabText }> { title } </Text>
+                </View>
+            </TouchableOpacity>
+        )
+    }
 
-    const [contador, setContador] = useState(0)
+    const android = () => {
+        return (
+            <View
+                style={[ 
+                    styles.fabLocation,  
+                    ( position === 'bl' ) ? styles.left : styles.right
+                ]}
+            >
+                <TouchableNativeFeedback
+                    onPress={ onPress }
+                    background={ TouchableNativeFeedback.Ripple('#28425B', false, 30 ) }
+                >
+                    <View style={ styles.fab }>
+                        <Text style={ styles.fabText }> { title } </Text>
+                    </View>
+                </TouchableNativeFeedback>
+            </View>
+        )
+    }
 
-    return (
-    <>
-    
-   <View 
-    style={(
-        position === 'bl' 
-        ? style.fabLocationBR
-        : style.fabLocationBL
-    )}
-   >
-     <TouchableNativeFeedback
-    onPress={onPress}
-    >
-    <View style={style.fab}>
-        <Text style={style.fabChildren}>
-            {title}
-        </Text>
-    </View>
-    </TouchableNativeFeedback>
-    
-    </View> 
 
-    </>
-  )
+
+    return (Platform.OS === 'ios') ? ios() : android();
 }
 
-const style = StyleSheet.create({
-    fabLocationBR: {
+const styles = StyleSheet.create({
+    fabLocation: {
         position: 'absolute',
         bottom: 25,
+    },
+    right: {
         right: 25
     },
-    fabLocationBL: {
-        position: 'absolute',
-        bottom: 25,
+    left: {
         left: 25
     },
     fab: {
-        backgroundColor: '#5856d6',
+        backgroundColor: '#5856D6',
         width: 60,
         height: 60,
         borderRadius: 100,
         justifyContent: 'center',
-        shadowColor: '#000',
-        textShadowOffset: {
+        shadowColor: "#000",
+        shadowOffset: {
             width: 0,
-            height: 4
+            height: 4,
         },
         shadowOpacity: 0.30,
         shadowRadius: 4.65,
-        elevation: 8
+
+        elevation: 8,
     },
-    fabChildren: {
+    fabText: {
         color: 'white',
-        fontSize: 20,
+        fontSize: 25,
         fontWeight: 'bold',
         alignSelf: 'center',
-        shadowColor: '#000',
-        textShadowOffset: {
-            width: 0,
-            height: 4
-        },
-        shadowOpacity: 0.30,
-        shadowRadius: 4.65,
-        elevation: 8
     }
 })
--- src/screens/ContadorScreen.tsx
+++ src/screens/ContadorScreen.tsx
@@ -1,77 +1,50 @@
-import {useState} from 'react'
-import { Fab } from '../components/Fab'
-import { View, Text, TouchableOpacity, StyleSheet, TouchableWithoutFeedback } from 'react-native'
+import React, { useState } from 'react'
+import { View, Text, StyleSheet } from 'react-native';
+import { Fab } from '../components/Fab';
 
 export const ContadorScreen = () => {
-  
-    const [contador, setContador] = useState(10)
 
-  return (
-    <View style={style.container}>
-    <Text style={style.title}>
-            Contador: {contador}
-    </Text>
-    
-    
-    
-    <Fab 
-        title='+1'
-        onPress={()=>setContador(contador+1)}
-    />
+    const [contador, setContador] = useState(10);
 
+    return (
+        <View style={ styles.container }>
+            <Text style={ styles.title }>
+                Contador: { contador }
+            </Text>
 
-    <Fab 
-        title='-1'
-        onPress={()=>setContador(contador-1)}
-    />
+            <Fab 
+                title="+1"
+                onPress={ () => setContador( contador + 1 ) }
+            />
 
-    {/* <TouchableOpacity
-    onPress={()=> setContador(contador-1)}
-    style={style.fabLocationBL}
-    >
-    <View style={style.fab}>
-        <Text style={style.fabChildren}>
-            -1
-        </Text>
-    </View>
-    </TouchableOpacity> */}
-    
-    </View>
-  )
+            <Fab 
+                title="-1"
+                position="bl"
+                onPress={ () => setContador( contador - 1 ) }
+            />
+
+            {/* <TouchableOpacity
+                style={ styles.fabLocationBL }
+                onPress={ () => setContador( contador - 1 ) }
+            >
+                <View style={ styles.fab }>
+                    <Text style={ styles.fabText }> -1 </Text>
+                </View>
+            </TouchableOpacity>
+             */}
+        </View>
+    )
 }
 
-const style = StyleSheet.create({
+
+const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center'
     },
     title: {
-        textAlign: 'center',
         fontSize: 40,
-        top: -15
+        top: -15,
+        textAlign: 'center',
     },
-    fabLocationBR: {
-        position: 'absolute',
-        bottom: 25,
-        right: 25
-    },
-    fabLocationBL: {
-        position: 'absolute',
-        bottom: 25,
-        left: 25
-    },
-    fab: {
-        backgroundColor: '#5856d6',
-        width: 60,
-        height: 60,
-        borderRadius: 100,
-        justifyContent: 'center'
-    },
-    fabChildren: {
-        color: 'white',
-        fontSize: 20,
-        fontWeight: 'bold',
-        alignSelf: 'center'
-    }
 })
-
